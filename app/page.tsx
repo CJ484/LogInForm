b5aef7dddf3db03a32e8f8d8c152f9fa8aef2ca2
cj484 <ccoreas484@gmail.com>
2023-12-01 17:28:59 -0500

    Refactor page component to use separate components for creating new user and user login

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,18 @@
 'use client';
-
-import { Button, Input } from "@mui/joy";
+import React from "react";
 import styles from "./page.module.scss";
-import Link from "next/link";
+import { CreateNewUser, UserLogIn } from "./Component";
 
-export default function Home() {
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted");
-  }
-  return (
+const Home = ()=> {
+    return (
     <main className={styles.main}>
       <h1 className={styles.title}>Authorization App</h1>
       <div>
-        <form className={styles.form} onSubmit={handleSubmit}>
-        <h2>Please Enter Your Credentials or</h2>
-        <Link href="#"><h2>Create A Free Account</h2></Link>
-        <Input className="form__input" placeholder="Username" />
-        <Input className="form__input" placeholder="Password"/>
-        <Button type="submit">Submit</Button>
-        </form>
+        <CreateNewUser />
+        <UserLogIn />
       </div>
     </main>
   );
 }
+
+export default Home;
