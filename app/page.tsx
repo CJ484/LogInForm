47eca70e8bd7c1fed0a3740dec396b4277843a7c
cj484 <ccoreas484@gmail.com>
2023-12-04 21:08:04 -0500

    Add global styles and update file paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./page.module.scss";
+import styles from "./Styles/page.module.scss";
 import Link from "next/link";
 
 const Home = () => {
