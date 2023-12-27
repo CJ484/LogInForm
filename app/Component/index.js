297d71a68881db1cf096e64f20e0d33a9d8511bb
cj484 <ccoreas484@gmail.com>
2023-12-26 23:10:15 -0500

    Add AuthenticateLogin component

--- app/Component/index.js
+++ app/Component/index.js
@@ -1,5 +1,6 @@
 import CreateNewUser from "./CreateNewUser";
 import UserLogIn from "./UserLogIn";
 import AddUser from "./AddUser";
+import AuthenticateLogin from "./AuthenticateLogin";
 
-export { CreateNewUser, UserLogIn, AddUser };
+export { CreateNewUser, UserLogIn, AddUser, AuthenticateLogin };
