3a14b682a1236629aea2fba3f2be035b27057349
cj484 <ccoreas484@gmail.com>
2023-12-29 17:08:59 -0500

    Add password visibility toggle and toast notifications to UserLogIn component

--- app/Component/UserLogIn/index.tsx
+++ app/Component/UserLogIn/index.tsx
@@ -1,13 +1,23 @@
 "use client";
 import Link from "next/link";
+import toast, { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
 import { AuthenticateLogin } from "..";
-import { Input, Button } from "@mui/joy";
+import { Button } from "@mui/joy";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  FormControl,
+  InputLabel,
+  IconButton,
+  InputAdornment,
+  FilledInput,
+} from "@mui/material";
 import BackButton from "@/app/assets/symbols/arrow_left";
 import styles from "../../Styles/page.module.scss";
 import { useRouter } from "next/navigation";
 
 const UserLogIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const [logInStatus, setLogInStatus] = useState(false);
@@ -24,17 +34,26 @@ const UserLogIn = () => {
       !AuthenticateLogin({ email: emailInput, password: passwordInput }) ===
       false
     ) {
-      console.log("User Log In Successful");
+      toast.success("Log In Successful");
       resetForm();
       setLogInStatus(true);
     } else {
-      console.log("User Log In Failed");
+      toast.error("Log In Failed");
     }
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
   };
 
   useEffect(() => {
     if (logInStatus === true) {
-      console.log("changing page");
       push("/pages/Home");
     }
   }, [logInStatus]);
@@ -42,29 +61,47 @@ const UserLogIn = () => {
   return (
     <>
       <form onSubmit={submission} className={styles.form}>
-      <Link className={styles.backButton} href="/"><BackButton /> Go back</Link>
+        <Link className={styles.backButton} href="/">
+          <BackButton /> Go back
+        </Link>
         <h2>Log In</h2>
         <a className={styles.links} href="../pages/register">
           <h3>or Create an Account</h3>
         </a>
-        <Input
-          required
-          className={styles.form_input}
-          placeholder="Email"
-          value={emailInput}
-          onChange={(e) => setEmailInput(e.target.value)}
-        />
-        <Input
-          required
-          className={styles.form_input}
-          placeholder="Password"
-          value={passwordInput}
-          onChange={(e) => setPasswordInput(e.target.value)}
-        />
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-email">Email</InputLabel>
+          <FilledInput
+            id="filled-adornment-email"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+          />
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+          <FilledInput
+            id="filled-adornment-password"
+            type={showPassword ? "text" : "password"}
+            value={passwordInput}
+            onChange={(e) => setPasswordInput(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
         <Button className={styles.form_button} type="submit">
           Log In
         </Button>
       </form>
+      <Toaster />
     </>
   );
 };
