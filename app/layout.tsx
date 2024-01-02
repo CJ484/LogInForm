422daf580015dc5cf8799f7ad859e158d8005792
cj484 <ccoreas484@gmail.com>
2024-01-02 15:29:34 -0500

    Refactor layout and page components with eslint corrections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import type { Metadata } from "next";
-import styles from "./Styles/page.module.scss";
-import "./Styles/globals.scss";
+import React from 'react';
+import type {Metadata} from 'next';
+import styles from './Styles/page.module.scss';
+import './Styles/globals.scss';
 export const metadata: Metadata = {
-  title: "Authorization App",
-  description: "A simple login form to demonstrate Next.js usage",
+	title: 'Authorization App',
+	description: 'A simple login form to demonstrate Next.js usage',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="A simple Login form to demonstrate my use of Next.js, Express.js, Node.js"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/x-icon" href='./assets/symbols/key.svg' />
-        <title>Authorization App</title>
-      </head>
-      <body className={styles.main}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta
+					name='description'
+					content='A simple Login form to demonstrate my use of Next.js, Express.js, Node.js'
+				/>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' type='image/x-icon' href='./assets/symbols/key.svg' />
+				<title>Authorization App</title>
+			</head>
+			<body className={styles.main}>
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
-"use client";
-import React from "react";
-import styles from "./Styles/page.module.scss";
-import Link from "next/link";
-import * as dotenv from "dotenv";
+'use client';
+import React from 'react';
+import styles from './Styles/page.module.scss';
+import Link from 'next/link';
+import * as dotenv from 'dotenv';
 
 const Home = () => {
-  dotenv.config();
-  return (
-    <main>
-      <h1 className={styles.title}>Authorization App</h1>
-      <div className={styles.navigation}>
-        <Link className={styles.nav_button} href="/pages/login">
+	dotenv.config();
+	return (
+		<main>
+			<h1 className={styles.title}>Authorization App</h1>
+			<div className={styles.navigation}>
+				<Link className={styles.nav_button} href='/pages/login'>
           Log in
-        </Link>{" "}
-        <Link className={styles.nav_button} href="/pages/register">
+				</Link>{' '}
+				<Link className={styles.nav_button} href='/pages/register'>
           Register
-        </Link>{" "}
-        <Link className={styles.nav_button} href="/pages/displaydata">
+				</Link>{' '}
+				<Link className={styles.nav_button} href='/pages/displaydata'>
           DisplayData
-        </Link>
-      </div>
-    </main>
-  );
+				</Link>
+			</div>
+		</main>
+	);
 };
 
 export default Home;
