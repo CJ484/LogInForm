c1f1d24c981ba935af4bbcd4d126ebbb8fc4bb83
cj484 <ccoreas484@gmail.com>
2024-01-29 11:30:50 -0500

    Refactor CreateNewUser Form component to use Material-UI components

--- app/Component/CreateNewUser/index.tsx
+++ app/Component/CreateNewUser/index.tsx
@@ -2,7 +2,12 @@
 import React, {type FormEvent} from 'react';
 import {AddUser} from '../../api';
 import {useState} from 'react';
-import {isEqual} from 'lodash';
+import {first, isEqual} from 'lodash';
+import {
+	FormControl,
+	InputLabel,
+	FilledInput,
+} from '@mui/material';
 import {Input, Button} from '@mui/joy';
 import toast, {Toaster} from 'react-hot-toast';
 import BackButton from '@/app/assets/symbols/arrow_left';
@@ -62,11 +67,48 @@ const CreateNewUser = () => {
 			.catch(error => error as Error);
 	};
 
+	const inputFields = [
+		{
+			placeholder: 'First Name',
+			value: firstNameInput,
+			onChange(e: React.ChangeEvent<HTMLInputElement>) {
+				setFirstNameInput(e.target.value);
+			},
+		},
+		{
+			placeholder: 'Last Name',
+			value: lastNameInput,
+			onChange(e: React.ChangeEvent<HTMLInputElement>) {
+				setLastNameInput(e.target.value);
+			},
+		},
+		{
+			placeholder: 'Email',
+			value: emailInput,
+			onChange(e: React.ChangeEvent<HTMLInputElement>) {
+				setEmailInput(e.target.value);
+			},
+		},
+		{
+			placeholder: 'Password',
+			value: passwordCypherInput,
+			onChange(e: React.ChangeEvent<HTMLInputElement>) {
+				setPasswordCypherInput(e.target.value);
+			},
+		},
+		{
+			placeholder: 'Confirm Password',
+			value: passwordConfirmInput,
+			onChange(e: React.ChangeEvent<HTMLInputElement>) {
+				setPasswordConfirmInput(e.target.value);
+			},
+		},
+	];
+
 	return (
 		<>
 			<form onSubmit={submission} className={styles.form}>
 				<div className={styles.blurBackground}></div>
-
 				<Link href='/' className={styles.backButton}>
 					<BackButton />
           Go back
@@ -74,55 +116,25 @@ const CreateNewUser = () => {
 				<h2 className={styles.form_title}>Creating an Account is Easy</h2>
 				<h3 className={styles.form_title}>Just fill out the form below!</h3>
 				<Link href='../pages/login' className={styles.links}>
-          Already have an Account? Log in Here!
+					Already have an Account? Log in Here!
 				</Link>
-				<Input
-					required
-					className={styles.form_input}
-					placeholder='First Name'
-					value={firstNameInput}
-					onChange={e => {
-						setFirstNameInput(e.target.value);
-					}}
-				/>
-				<Input
-					required
-					className={styles.form_input}
-					placeholder='Last Name'
-					value={lastNameInput}
-					onChange={e => {
-						setLastNameInput(e.target.value);
-					}}
-				/>
-				<Input
-					required
-					className={styles.form_input}
-					placeholder='Email'
-					value={emailInput}
-					onChange={e => {
-						setEmailInput(e.target.value);
-					}}
-				/>
-				<Input
-					required
-					className={styles.form_input}
-					placeholder='Password'
-					value={passwordCypherInput}
-					onChange={e => {
-						setPasswordCypherInput(e.target.value);
-					}}
-				/>
-				<Input
-					required
-					className={styles.form_input}
-					placeholder='Confirm Password'
-					value={passwordConfirmInput}
-					onChange={e => {
-						setPasswordConfirmInput(e.target.value);
-					}}
-				/>
+				{inputFields.map((input, index) => (
+					<FormControl key={index} sx={{m: 1, width: '25ch'}} variant='filled'>
+						<InputLabel className={styles.form_input} htmlFor='filled-adornment-email'>{input.placeholder}</InputLabel>
+						<FilledInput
+							className={styles.form_input}
+							required
+							id={`filled-adornment-${input.placeholder}`}
+							value={input.value}
+							onChange={e => {
+								if (e.target instanceof HTMLInputElement) {
+									input.onChange(e as React.ChangeEvent<HTMLInputElement>);
+								}
+							}}
+						/>
+					</FormControl>))}
 				<Button className={styles.form_button} type='submit'>
-          Create
+					Create
 				</Button>
 			</form>
 			<Toaster />
