92b74397618ddeb0925dc896d33ac222c2824ff8
cj484 <ccoreas484@gmail.com>
2024-01-28 23:26:58 -0500

    Refactor DisplayDataPage component

--- app/pages/displaydata/page.tsx
+++ app/pages/displaydata/page.tsx
@@ -1,21 +1,25 @@
+/* eslint-disable arrow-body-style */
 'use client';
 import React from 'react';
 import {DisplayVisitedUsers} from '@/app/Component';
 import Link from 'next/link';
-import DisplayUsers from '../../api/DisplayUsers';
 import BackButton from '../../assets/symbols/arrow_left';
 import styles from '../../Styles/page.module.scss';
 
-const DisplayDataPage = () => (
-	<div className={styles.displayDatapage}>
-		<Link href='/' className={styles.backButton2}>
-			<BackButton />
-			<p>Go Back</p>
-		</Link>
-		<h1>Users who have visited</h1>
-		<h2>Thank You for seeing my work</h2>
-		<DisplayVisitedUsers />
-	</div>
-);
+const DisplayDataPage = () => {
+	return (
+		<div className={styles.DisplayUsersPage}>
+			<Link href='/' className={styles.backButton2}>
+				<BackButton />
+				<p>Go Back</p>
+			</Link>
+			<h1>Users who have visited</h1>
+			<h2>Thank You for seeing my work</h2>
+			<div className={styles.visitedUsers}>
+				<DisplayVisitedUsers />
+			</div>
+		</div>
+	);
+};
 
 export default DisplayDataPage;
